b9475c30acd2bbcaa7f6cfa0c1249ee560d4807a
Kartik Kumar <83923172+kartik11721@users.noreply.github.com>
2021-10-28 22:07:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-def add(x, y):
-    return x + y
+from tkinter import *
+root = Tk()
+root.geometry('400x180')
+root.title('Calculator')
 
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
-
-while True:
-    # take input from the user
-    operation = input("Enter The Operation (+,-,*,/): ")
-
-    if operation in ('+', '-', '*', '/'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if operation == '+':
-            print(f"{num1} + {num2} = {num1 + num2}")
-
-        elif operation == '-':
-            print(f"{num1} - {num2} = {num1 - num2}")
-
-        elif operation == '*':
-            print(f"{num1} * {num2} = {num1 * num2}")
-
-        elif operation == '/':
-            print(f"{num1} / {num2} = {num1 / num2}")
-        break
+def execute():
+    try:
+        number1 = float(n1.get())
+        number2 = float(n2.get())
+        operation = op.get()
     
-    else:
-        print("Please Enter A Valid Input")
+        if operation == '+':
+            answer = f"{number1} + {number2} = {number1 + number2}"
+        elif operation == '-':
+            answer = f"{number1} - {number2} = {number1 - number2}"
+        elif operation == '*':
+            answer = f"{number1} * {number2} = {number1 * number2}"
+        elif operation == '/':
+            answer = f"{number1} / {number2} = {number1 / number2}"
+        else:
+            Label(root, text = 'Invalid Operation', fg = 'red').grid(row = 4, column = 1)
+        Label(root, text = answer).grid(row = 4, column = 1)
+    except:
+        Label(root, text = 'Invalid Input', fg = 'red').grid(row = 4, column = 1)
+
+Label(root, text = "Enter first number").grid(row = 0, column = 0, pady =5)
+n1 = Entry(root, width= 30)
+n1.grid(row = 0, column = 1)
+Label(root, text = "Enter second number").grid(row = 1, column = 0, pady =5)
+n2 = Entry(root, width= 30)
+n2.grid(row = 1, column = 1)
+Label(root, text = "Enter The Operation (+,-,*,/): ").grid(row = 2, column = 0, pady =5)
+op = Entry(root, width= 30)
+op.grid(row = 2, column = 1)
+
+Button(root, text = 'Confirm',command = execute).grid(row = 3, column = 1, padx =10, pady =10)
+
+root.mainloop()
